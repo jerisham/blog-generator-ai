53eacdaea1b4497728d1a6f91ddca24f749d58a3
jerisha <jerishajoseph4@gmail.com>
2026-06-06 23:20:59 +0530

    Add debug_env path to urlpatterns

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,9 @@
-
-# urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('debug-env/', views.debug_env, name='debug_env'),  # ← add this
     path('api/generate-blog/', views.generate_blog, name='generate_blog'),
     path('api/download-pdf/', views.download_pdf, name='download_pdf'),
     path('api/seo-analyze/', views.seo_analyze, name='seo_analyze'),
